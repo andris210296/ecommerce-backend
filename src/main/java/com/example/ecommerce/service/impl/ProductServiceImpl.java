15fc731a96a4cfe4b7211538e274dd39587c924c
Andris Andermann <andris.andermann@terra.com.br>
2024-07-29 16:20:39 -0300

    Updates on ProductService

---
+++ src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.ecommerce.service.impl;
+
+import com.example.ecommerce.dto.ProductDto;
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.repository.ProductRepository;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElse(null);
+    }
+
+    @Override
+    public Product createProduct(ProductDto productDto) {
+        Product product = productDto.toProduct();
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Long id, ProductDto productDetails) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            product.setName(productDetails.name());
+            product.setPrice(productDetails.price());
+            product.setDescription(productDetails.description());
+            return productRepository.save(product);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean deleteProduct(Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
